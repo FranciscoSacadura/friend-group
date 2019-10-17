01fa4c3067424b781fb9b70180cb2984ffe3c9bf
Francisco Sacadura <zcbt003@ucl.ac.uk>
2019-10-17 11:43:13 +0100

    part 1

--- group.py
+++ group.py
@@ -2,4 +2,12 @@
 
 # Your code to go here...
 
-my_group =
+Jill = {'name':'Jill', 'age':26, 'job':'biologist', 'relationships':{'Zalika':'friend','John':'partner'}}
+
+Zalika = {'name':'Zalika', 'age':28, 'job':'artist', 'relationships':{'Jill':'friend'}}
+
+John = {'name':'John', 'age':27, 'job':'writer', 'relationships':{'Jill':'partner'}}
+
+Nash = {'name':'Nash', 'age':34, 'job':'chef', 'relationships':{'John':'cousin','Zalika':'landlord'}}
+
+my_group = {'Jill':Jill,'Zalika':Zalika,'John':John,'Nash':Nash}
